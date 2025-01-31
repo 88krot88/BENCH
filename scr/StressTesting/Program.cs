0097c8e8c56c3145835461c7ea2de273ef861b9d
Панишева Дарья <dasha.panishewa1@yandex.ru>
2025-01-31 13:34:53 +0700

    работа над ошибками

--- scr/StressTesting/Program.cs
+++ scr/StressTesting/Program.cs
@@ -1,8 +1,38 @@
-﻿class Program
+﻿using Bench;
+using Microsoft.VisualBasic.Devices;
+using System.Diagnostics;
+
+class Program
 {
-    static void Main()
-    {
-        var stressTest = new StressTest();
-        stressTest.Run();
-    }
+	/// <summary>
+	/// Точка входа.
+	/// </summary>
+	/// <param name="args">Аргументы.</param>
+	private static void Main(string[] args)
+	{
+		var stopWatch = new Stopwatch();
+		var benchParameters = new BenchParameters(150, 20, 30, 40, 40);
+		var compassWrapper = new BenchWrapper();
+		var benchBuilder = new BenchBuilder(compassWrapper);
+
+		var streamWriter = new StreamWriter("log.txt", true);
+		var count = 0;
+		while (true)
+		{
+			stopWatch.Start();
+			const double gigabyteInByte = 0.000000000931322574615478515625;
+			compassWrapper.ConnectToKompas();
+			benchBuilder.BuildBench(benchParameters);
+
+			var computerInfo = new ComputerInfo();
+			var usedMemory = (computerInfo.TotalPhysicalMemory
+							  - computerInfo.AvailablePhysicalMemory)
+							 * gigabyteInByte;
+			var logEntry = $"{++count}\t{stopWatch.Elapsed:hh\\:mm\\:ss}\t{usedMemory}";
+			streamWriter.WriteLine(logEntry);
+			Console.WriteLine(logEntry);
+			streamWriter.Flush();
+			stopWatch.Reset();
+		}
+	}
 }
